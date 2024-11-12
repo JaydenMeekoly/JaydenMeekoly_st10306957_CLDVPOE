7f090e90137a15b942ef45c21c6c05acc8fa5122
JaydenMeekoly <129750476+JaydenMeekoly@users.noreply.github.com>
2024-11-12 20:56:14 +0200

    Add files via upload

---
+++ Services/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using CLDVPOE2.Models;
+using System.Data.SqlClient;
+
+namespace CLDVPOE2.Services
+{
+    public class CustomerService
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomerService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task InsertCustomerAsync(CustomerProfile profile)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var query = @"INSERT INTO CustomerTable (FirstName, SecondName, Email, PhoneNumber)
+                          VALUES (@FirstName, @SecondName, @Email, @PhoneNumber)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@FirstName", profile.FirstName);
+                command.Parameters.AddWithValue("@SecondName", profile.LastName);
+                command.Parameters.AddWithValue("@Email", profile.Email);
+                command.Parameters.AddWithValue("@PhoneNumber", profile.PhoneNumber);
+
+                connection.Open();
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
